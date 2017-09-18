6d9d384e9e7ca137a5a77c6c7302602940dd93b0
Lee-Zard <afj@wp.pl>
2017-09-18 22:17:41 +0200

    Adding first commit

---
+++ Android/README.md
@@ -0,0 +1,2 @@
+"# WarhammerAndroid" 
+"# WarhammerAndroid" 
---
+++ Warhammer/app/src/main/java/com/leezard/warhammer/WarhammerBrowser.java
@@ -0,0 +1,13 @@
+package com.leezard.warhammer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WarhammerBrowser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_warhammer_browser);
+    }
+}
